3c8d165583b44d8cccd5a16b27dc14d78efd8f4e
ShujaShah <shuja.ashraf11@gmail.com>
2022-06-11 16:43:36 +0530

    creating or adding the data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import axios from "axios";
 
+const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
 class App extends Component {
   state = {
     posts: [],
@@ -9,14 +11,19 @@ class App extends Component {
 
   async componentDidMount() {
     // pending -> resolved (success) or rejected (failure)
-    const { data: posts } = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
+    const { data: posts } = await axios.get(apiEndpoint);
     this.setState({ posts });
   }
 
-  handleAdd = () => {
-    console.log("Add");
+  handleAdd = async () => {
+    //console.log("Add");
+    const obj = {
+      title: "This is Shuja Ashraf Shah",
+      body: "Software Engineer....",
+    };
+    const { data: post } = await axios.post(apiEndpoint, obj);
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
   };
 
   handleUpdate = (post) => {
